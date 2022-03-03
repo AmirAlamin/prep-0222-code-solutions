31bd0231bcd329cbb0ec2d74f268bd4c0d042546
AmirAlamin <princeamirr2@gmail.com>
2022-03-03 03:44:45 +0000

    completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,15 @@
+const language =
+['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(language);
+
+var firstelement = 'the first element of the array is html';
+console.log(firstelement);
+
+var thirdelement = 'the third element of the arrat is javascript';
+console.log(thirdelement);
+
+var length = 'the length of the array is 6';
+console.log(length);
+
+var lastelement = 'the last element of the array is postgres';
+console.log(lastelement);
